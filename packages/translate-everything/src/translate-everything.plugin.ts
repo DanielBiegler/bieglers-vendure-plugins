56f04163887c94697db48ee37e2a383e7f053476
Daniel Biegler <DanielBiegler@users.noreply.github.com>
2025-05-04 15:16:29 +0200

    import translations

--- packages/translate-everything/src/translate-everything.plugin.ts
+++ packages/translate-everything/src/translate-everything.plugin.ts
@@ -1,9 +1,11 @@
-import { PluginCommonModule, VendurePlugin } from "@vendure/core";
+import { OnApplicationBootstrap } from "@nestjs/common";
+import { I18nService, PluginCommonModule, VendurePlugin } from "@vendure/core";
 import { adminSchema } from "./api/api-extensions";
 import { TranslateEverythingAdminResolver } from "./api/translate-everything.resolver";
 import { PermissionTranslateEverything, PermissionTranslateEverythingEntry, PLUGIN_INIT_OPTIONS } from "./constants";
 import { TranslateEverythingEntryProduct } from "./translate-everything-entry.entity";
 import { TranslateEverythingService } from "./translate-everything.service";
+import { en } from "./translations/en";
 import { PluginTranslateEverythingOptions } from "./types";
 
 /**
@@ -32,9 +34,15 @@ import { PluginTranslateEverythingOptions } from "./types";
   },
   compatibility: ">=3.0.0",
 })
-export class TranslateEverythingPlugin {
+export class TranslateEverythingPlugin implements OnApplicationBootstrap {
   /** @internal */
   static options: PluginTranslateEverythingOptions;
+
+  constructor(private i18nService: I18nService) {}
+
+  onApplicationBootstrap() {
+    this.i18nService.addTranslation("en", en);
+  }
 
   /**
    * The static `init()` method is called with the options to configure the plugin.
