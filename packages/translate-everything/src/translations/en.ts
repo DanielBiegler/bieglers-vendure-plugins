98803f33f79aadd961031f7fe6e534d1b16a54bf
Daniel Biegler <DanielBiegler@users.noreply.github.com>
2025-05-04 15:14:03 +0200

    export translation

--- packages/translate-everything/src/translations/en.ts
+++ packages/translate-everything/src/translations/en.ts
@@ -1,19 +1,21 @@
-{
-  "pluginTranslateEverything": {
-    "modal": {
-      "bulkTranslateLabel": "Have items translated",
-      "bulkTranslateFacetAndFacetValuesHeadline": "Enqueue {countFacets} facets and their facet values for translation?",
-      "bulkTranslateFacetAndFacetValuesBody": "This will add your selected facets and their facet values into a job queue for further processing."
+export const en = {
+  pluginTranslateEverything: {
+    modal: {
+      bulkTranslateLabel: "Have items translated",
+      bulkTranslateFacetAndFacetValuesHeadline: "Enqueue {countFacets} facets and their facet values for translation?",
+      bulkTranslateFacetAndFacetValuesBody:
+        "This will add your selected facets and their facet values into a job queue for further processing.",
     },
-    "action": {
-      "translateFacette": "Translate Facette"
+    action: {
+      translateFacette: "Translate Facette",
     },
-    "menu": {
-      "labelMenu": "Translation Service",
-      "labelOverview": "Overview"
+    menu: {
+      labelMenu: "Translation Service",
+      labelOverview: "Overview",
     },
-    "error": {
-      "sourceLanguageNotFound": "Product(id={productId}) has no source ProductTranslation with LanguageCode.{sourceLanguage}"
-    }
-  }
-}
+    error: {
+      sourceLanguageNotFound:
+        "Product(id={productId}) has no source ProductTranslation with LanguageCode.{sourceLanguage}",
+    },
+  },
+};
