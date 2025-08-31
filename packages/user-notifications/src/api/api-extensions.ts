f3b1117bcd6e4f248e20c6b13bb875810e655b89
Daniel Biegler <DanielBiegler@users.noreply.github.com>
2025-08-31 13:22:19 +0200

    add api extension

--- packages/user-notifications/src/api/api-extensions.ts
+++ packages/user-notifications/src/api/api-extensions.ts
@@ -1,19 +1,60 @@
 import { gql } from "graphql-tag";
 
 export const adminApiExtensions = gql`
+  type UserNotification implements Node {
+    id: ID!
+    createdAt: DateTime!
+    updatedAt: DateTime!
+    
+    "For potentially displaying a thumbnail/banner in the notification or attaching a file"
+    asset: Asset
+    "Used for ordering, would also be the time for a changelog"
+    dateTime: DateTime
+    "Headline or title of the notification"
+    title: String!
+    "Main content of the notification, depending on your needs you might want to use plain text, markdown, something else"
+    content: String
+    "This gets resolved for the active user"
+    readAt: DateTime
+  }
 
-  # In case you need a field resolver for result unions
-  #
-  # type PluginUserNotificationsResult {
-  #   # TODO
-  # }
-  # union PluginUserNotificationsCreateResult = Asset | PluginUserNotificationsResult
+  type UserNotificationList implements PaginatedList {
+    items: [UserNotification!]!
+    totalItems: Int!
+  }
+  
+  input UserNotificationListOptions;
 
   extend type Query {
-    # TODO
+    userNotification(id: ID!): UserNotification!
+    "List all notifications for the active user, by default orders by dateTime descending"
+    userNotificationList(options: UserNotificationListOptions): UserNotificationList!
+  }
+
+  input PluginUserNotificationCreateInput {
+    title: String!
+    dateTime: DateTime
+    content: String
+    imageId: ID
+  }
+
+  input PluginUserNotificationUpdateInput {
+    id: ID!
+    title: String
+    dateTime: DateTime
+    content: String
+    imageId: ID
+  }
+
+  input UserNotificationMarkAsReadInput {
+    ids: [ID!]!
+    userId: ID
   }
 
   extend type Mutation {
-    # TODO
+    userNotificationCreate(input: PluginUserNotificationCreateInput!): UserNotification!
+    userNotificationUpdate(input: PluginUserNotificationUpdateInput!): UserNotification!
+    userNotificationDelete(ids: [ID!]!): DeletionResponse!
+    userNotificationMarkAsRead(input: UserNotificationMarkAsReadInput!): [UserNotification!]!
   }
 `;
