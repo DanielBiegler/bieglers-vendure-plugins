1c929a235413c2283cf024d40d4c1b14310ccc52
Daniel Biegler <DanielBiegler@users.noreply.github.com>
2025-09-07 21:58:51 +0200

    update package exports

--- packages/channel-notifications/src/index.ts
+++ packages/channel-notifications/src/index.ts
@@ -7,7 +7,24 @@
  * - events
  * - custom strategies that can be configured by the user of the plugin
  */
+
 export { ChannelNotificationsPlugin } from "./plugin";
 export { ChannelNotificationsOptions } from "./types";
 
+export {
+  // Currently not sure if these should be exported. Let's leave em blank for now.
+  // CustomChannelNotificationFields,
+  // CustomChannelNotificationFieldsTranslation,
+  // CustomChannelNotificationReadReceiptFields,
+  ChannelNotification,
+  ChannelNotificationReadReceipt,
+  ChannelNotificationTranslation
+} from "./entities/channel-notification.entity";
+
 export { ChannelNotificationsService } from "./services/main.service";
+
+export {
+  ChannelNotificationEvent,
+  ChannelNotificationEventInput,
+  ChannelNotificationEventMarkedAsRead
+} from "./events";
