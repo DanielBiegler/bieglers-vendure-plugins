71db000beabbb38594dab1bd6a4323d06f30a88a
Daniel Biegler <DanielBiegler@users.noreply.github.com>
2026-04-28 12:43:55 +0200

    rename variables

--- packages/invoices/src/constants.ts
+++ packages/invoices/src/constants.ts
@@ -6,8 +6,8 @@ export const loggerCtx = "PluginInvoices";
 /** @internal */
 export const PLUGIN_INIT_OPTIONS = Symbol("PLUGIN_INIT_OPTIONS");
 
-export const permissionsCrud = new CrudPermissionDefinition("invoice")
-export const permissionsConfig = new PermissionDefinition({ name: "invoice-config" }) // TODO description?
+export const InvoicePermissions = new CrudPermissionDefinition("invoice")
+export const InvoiceConfigPermissions = new PermissionDefinition({ name: "invoice-config" }) // TODO description?
 
 /**
  * # TODO this should be thoroughly documented and mentioned in README!
