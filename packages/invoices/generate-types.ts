054c2d8157ee843c2624adf09ec7f5d42e9fe7e9
Daniel Biegler <DanielBiegler@users.noreply.github.com>
2026-04-28 12:43:23 +0200

    generate types

--- packages/invoices/generate-types.ts
+++ packages/invoices/generate-types.ts
@@ -7,7 +7,18 @@ require("dotenv").config({ path: path.join(__dirname, "../dev-server/.env") });
 generateTypes(
   {
     plugins: [
-      InvoicesPlugin.init({}),
+      InvoicesPlugin.init({
+        // @ts-ignore not needed for type generation
+        invoiceIdPrefixGenerationStrategy: undefined,
+        // @ts-ignore not needed for type generation
+        creditNoteIdPrefixGenerationStrategy: undefined,
+        // @ts-ignore not needed for type generation
+        invoiceFileGenerationStrategy: undefined,
+        // @ts-ignore not needed for type generation
+        creditNoteFileGenerationStrategy: undefined,
+        // @ts-ignore not needed for type generation
+        storageStrategy: undefined
+      }),
     ],
   },
   {
